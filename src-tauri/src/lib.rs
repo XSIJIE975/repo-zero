88fad1fa145e21b0df6ae5b11936108a9d6d5800
XSIJIE975 <xsijie975@qq.com>
2026-01-30 16:16:43 +0800

    chore: add multi-platform release workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -445,6 +445,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![greet, execute_reset, scan_repo, check_git_status])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
